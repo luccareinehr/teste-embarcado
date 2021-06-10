45ddef29666333fdd034ab920a79375345c2bfb0
Clang-Format <>
2021-06-10 18:15:10 +0000

    Apply clang-format

--- sampling.cpp
+++ sampling.cpp
@@ -3,33 +3,29 @@
 
 // Libraries
 #include "sampling.hpp"
+
 #include <stdint.h>
 #include <stdlib.h>
+
 #include "ACS712.h"
 #include "ACS712_fix.hpp"
 
 int16_t getma(ACS712 acs, uint16_t samples) {
-    int16_t avg = 0; int32_t total = 0; // total value
-        int16_t mamp; // value in mA
+    int16_t avg = 0;
+    int32_t total = 0;  // total value
+    int16_t mamp;       // value in mA
 
-    
-    
-    
     // Capturing all samples
-    for (int i = 0; i < samples; i++) 
-    {
+    for (int i = 0; i < samples; i++) {
         mamp = acs.mA_DC();  // Get data from sensor (in miliamps)
         total += mamp;       // Add current reading to total
     }
 
-    
-    
-    
-    
     // Divides sum of all readings by the number of samples (average)
     // and returns the absolute value
     avg = total / samples;
     if (avg >= 0) {
         return avg;
-    } else return -avg;
+    } else
+        return -avg;
 }
