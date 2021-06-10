437e852a8b4904f221d594655f58bbfa754a40fa
luccareinehr <75084099+luccareinehr@users.noreply.github.com>
2021-06-10 15:07:42 -0300

    Update sampling.cpp

--- sampling.cpp
+++ sampling.cpp
@@ -3,30 +3,33 @@
 
 // Libraries
 #include "sampling.hpp"
-
 #include <stdint.h>
-
+#include <stdlib.h>
 #include "ACS712.h"
+#include "ACS712_fix.hpp"
 
-// Captures samples from the ACS712 and averages them
-// Pass the ACS712 object and how many samples to take as parameters
 int16_t getma(ACS712 acs, uint16_t samples) {
-    // Reset total and avg variables for next reading
-    int16_t avg = 0;
-    int32_t total = 0;
-    int16_t mamp;
+    int16_t avg = 0; int32_t total = 0; // total value
+        int16_t mamp; // value in mA
 
+    
+    
+    
     // Capturing all samples
-    for (int i = 0; i < samples; i++) {
+    for (int i = 0; i < samples; i++) 
+    {
         mamp = acs.mA_DC();  // Get data from sensor (in miliamps)
         total += mamp;       // Add current reading to total
     }
 
+    
+    
+    
+    
     // Divides sum of all readings by the number of samples (average)
     // and returns the absolute value
     avg = total / samples;
     if (avg >= 0) {
         return avg;
-    } else
-        return -avg;
+    } else return -avg;
 }
